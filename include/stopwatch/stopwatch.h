2b474803f7daea3cdfcf250e743375a1eabf0b23
Sergey Kolesnik <sergey.kolesnik.ld@gmail.com>
2020-12-03 19:02:38 +0500

    added std-chrono-independent implementation

---
+++ include/stopwatch/stopwatch.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+
+// #include <chrono>
+
+namespace eld
+{
+    template <typename ClockType,
+            typename DurationCaster>
+    class stopwatch
+    {
+    public:
+
+        using clock_type = ClockType;
+        using duration = typename clock_type::duration;
+
+        stopwatch()
+             : start_(clock_type::now())
+        {}
+
+        void reset()
+        {
+            start_ = clock_type::now();
+        }
+
+        typename clock_type::duration result() const
+        {
+            return clock_type::now() - start_;
+        }
+
+        template<typename Duration>
+        Duration result() const
+        {
+            // return std::chrono::duration_cast<Duration>(result()); // TODO: remove this
+            return DurationCaster()(Duration(), result());
+        }
+
+        template<typename Duration>
+        operator Duration() const
+        {
+            return result<Duration>();
+        }
+
+        // This is ugly and does not work
+//        template <typename Caster>
+//        auto operator()(Caster caster) -> decltype(Caster::operator()(typename clock_type::time_point))
+//        {
+//            return caster(result());
+//        }
+
+    private:
+        typename clock_type::time_point start_;
+    };
+
+}
+
+
---
+++ tests/dummy.cpp
@@ -0,0 +1,39 @@
+﻿
+#include <stopwatch/stopwatch.h>
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+// this is inconvenient
+struct DurationCaster
+{
+    template<typename ToDuration, typename FromDuration>
+    constexpr ToDuration operator()(ToDuration, FromDuration fromDuration) const
+    {
+        return std::chrono::duration_cast<ToDuration>(fromDuration);
+    }
+};
+
+int main()
+{
+
+    eld::stopwatch<std::chrono::steady_clock, DurationCaster> steadyWatch{};
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+
+    std::chrono::nanoseconds nanoseconds = steadyWatch.result();
+    std::cout << nanoseconds.count() << std::endl;
+
+    std::chrono::milliseconds milliseconds = steadyWatch;
+    std::cout << milliseconds.count() << std::endl;
+
+    // This is ugly and does not work
+//    milliseconds = steadyWatch([](typename decltype(steadyWatch)::duration from)
+//            {
+//        return std::chrono::duration_cast<std::chrono::milliseconds>(from);
+//            });
+
+    return 0;
+}
+
